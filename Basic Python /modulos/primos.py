5ac10f90e6a5e5cc2c3e9de36cce48e154654b9c
Daniel <94924643+Darx51@users.noreply.github.com>
2025-04-24 13:10:31 -0500

    primo

--- Basic Python /modulos/primos.py
+++ Basic Python /modulos/primos.py
@@ -1,6 +1,16 @@
 def opcion_dos():
-     while True :
+    while True :
         try :
+
+            #Metodo a llamar            
+            def es_primo(numero): 
+                if numero<1:
+                    return False
+                for i in range(2, int(numero ** 0.5)+1): #
+                    if numero % i == 0:
+                        return False
+                return True
+
             #Metodo a llamar            
             def es_primo(numero): 
                 if numero<1:
